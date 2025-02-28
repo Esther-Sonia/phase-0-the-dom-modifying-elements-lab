0947aebc692c7d0352067c339f58f7805bf8ae57
Esther-Sonia <esthersonia21@gmail.com>
2025-02-28 09:54:33 +0300

    updates elements in the DOM

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your code here!
+
+const main = document.getElementById("main");
+if (main) {
+  main.remove();
+}
+
+const newHeader = document.createElement("h1");
+
+
+newHeader.id = "victory";
+
+
+newHeader.innerHTML = "Esther is the champion"; 
+
+
+document.body.append(newHeader);
